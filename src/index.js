91c87291ea7b3e3738fb7db48e74d53252a5639c
Apolinar Linares <41924872+apolinares@users.noreply.github.com>
2025-06-18 11:46:51 -0400

    cambios hasta la clase 5

--- src/index.js
+++ src/index.js
@@ -3,12 +3,12 @@ const config = require('./config');
 console.log('>>> Config leída:', config);
 const MongoProductRepository = require('./infraestructure/repositories/MongoProductRepository');
 const MongoCustomerRepository = require('./infraestructure/repositories/MongoCustomerRepository');
-/*const MySQLProductRepository = require('./infraestructure/repositories/MySQLProductRepository');*/
+const MySQLProductRepository = require('./infraestructure/repositories/MySQLProductRepository');
 const ProductController = require('./adapters/controllers/ProductController');
 const CustomerController = require('./adapters/controllers/CustomerController');
 const productRoutes = require('./adapters/routes/productRoutes');
 const customerRoutes = require('./adapters/routes/customerRoutes');
-/*const { verifyToken } = require('./adapters/middlewares/authJwt');
+const { verifyToken } = require('./adapters/middlewares/authJwt');
 const swaggerUI = require('swagger-ui-express');
 const swaggerSpec = require('./infraestructure/docs/swaggerConfig');
 const MongoUserRepository     = require('./infraestructure/repositories/MongoUserRepository');
@@ -17,24 +17,26 @@ const TokenGenerator          = require('./infraestructure/services/TokenGenerat
 const SignIn                  = require('./application/useCases/SignIn');
 const authRoutes              = require('./adapters/routes/authRoutes');
 const userRoutes          = require('./adapters/routes/userRoutes');
-const SignUp              = require('./application/useCases/SignUp');*/
+const SignUp              = require('./application/useCases/SignUp');
 
 const app = express();
 const port = config.port;
 // Middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
 // Dependencies
 const dbType = config.DB_TYPE || 'mongodb'; // 'mongo' o 'mysql'
 let productRepository, customerRepository;
 console.log('>>> DB_TYPE:', dbType);
+
 if (dbType === 'mysql') {
   productRepository = new MySQLProductRepository();
 } else {
   productRepository = new MongoProductRepository();
   customerRepository = new MongoCustomerRepository();
 }
-/*// —– SETUP AUTH —–
+// —– SETUP AUTH —–
 const userRepo       = new MongoUserRepository();
 const passwordHasher = new PasswordHasher();
 const tokenGen       = new TokenGenerator();
@@ -43,17 +45,19 @@ app.use('/api/v1/auth', authRoutes(signInUseCase));
 
 // ——— SETUP SIGNUP ———
 const signUpUseCase = new SignUp(userRepo, passwordHasher);
-app.use('/api/v1/users',express.json(),userRoutes(signUpUseCase));*/
+app.use('/api/v1/users',express.json(),userRoutes(signUpUseCase));
 
 const productController = new ProductController(productRepository);
 const customerController = new CustomerController(customerRepository);
+
 // Configuración de Swagger UI
-//app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerSpec));
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerSpec));
 
 // Routes
-//app.use('/api/v1/products', verifyToken, productRoutes(productController));
-app.use('/api/v1/products', productRoutes(productController));
-app.use('/api/v1/customers', customerRoutes(customerController));
+app.use('/api/v1/products', verifyToken, productRoutes(productController));
+app.use('/api/v1/customers', verifyToken, customerRoutes(customerController));
+//app.use('/api/v1/products', productRoutes(productController));
+//app.use('/api/v1/customers', customerRoutes(customerController));
 
 // Error Handling
 app.use((err, req, res, next) => {
